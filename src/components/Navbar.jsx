420bdf210a28ce2a6ba8f0efc1e3ff6ca208468b
Jazztin Chuqui Contreras <cjazztin7@gmail.com>
2026-07-07 14:25:32 -0500

    fix: Cambiar Link por a tag para boton Admin

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -32,10 +32,10 @@ const Navbar = () => {
                 <div className="storefront-actions">
 
                     {/* Botón Discreto de Vista Administrador */}
-                    <Link to="https://admin.sistematextil.pp.ua" className="admin-ghost-btn" title="Acceso exclusivo para personal">
+                    <a href="https://admin.sistematextil.pp.ua" className="admin-ghost-btn" title="Acceso exclusivo para personal">
                         <Shield size={14} />
                         <span>Admin</span>
-                    </Link>
+                    </a>
 
                     <div className="navbar-divider"></div>
 
